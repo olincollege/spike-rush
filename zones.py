e925f10272b0666b1fd7ce3a214f8d45159e0da5
Aditi Vinod <aditi.b.vinod@gmail.com>
2022-05-06 06:36:27 -0400

    Adds gun implementation to the agent controller (UNTESTED as of now; likely broken) & updates the initialization method in the zones class.

--- zones.py
+++ zones.py
@@ -10,29 +10,34 @@ class Zone:
     Class docstring
     """
 
-    def __init__(self, x_0, y_0, x_dim, y_dim):
+    def __init__(self,x_0, y_0, x_dim, y_dim):
         self._color = (180, 180, 180) # random test color
         self._top = x_0
         self._left = y_0
         self._width = x_dim
         self._height = y_dim
 
-    def initialize_zone(self, surface):
-        pygame.draw.rect(surface, self._color,
-            pygame.Rect(self._top, self._left, self._width, self._height))
+        self._area = pygame.Surface([self._width, self._height])
+
+        # set wall spawn coordinates to xpos and ypos
+        self.rect = self._area.get_rect()
+        self.rect.x = x_0
+        self.rect.y = y_0
 
     def in_zone(self, agent):
         if agent.location[0] < self._width:
             pass
 
-class PlantZone(Zone):
-    """
-    Zone where the spike can be planted.
-    """
-
 class SpawnZone(Zone):
     """
     Zone where players can spawn.
+    """
+    def __init__(self):
+        Zone.__init__(self, )
+
+class PlantZone(Zone):
+    """
+    Zone where the spike can be planted.
     """
 
 class DamageZone(Zone):
