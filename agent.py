3aae16179bf56443854e6b289deccb3680f55468
Aditi Vinod <aditi.b.vinod@gmail.com>
2022-05-07 14:05:00 -0400

    Adds the most updated game loop version.

--- agent.py
+++ agent.py
@@ -11,6 +11,8 @@ from abc import ABC, abstractmethod
 from spike import *
 from spike_map import *
 import math
+from hud import display_model, display_view
+
 
 class Agent:
     """
@@ -492,3 +494,97 @@ class AgentController:
     def orb_interaction(self, event):
         # Not implementing rn
         pass
+
+def agent_test():
+    """
+    Tests movement code with a test character.
+    """
+    pygame.init()  # initialize pygame
+    map_model = split_model()
+    map_view = split_view(map_model)  # initialize map
+    clock = pygame.time.Clock()  # to keep track of time in-game
+    character_speed = 10
+  
+    track_second = pygame.USEREVENT  # using ID 24
+
+    # trigger event every second
+    pygame.time.set_timer(track_second, 1000)
+
+    # Player 1 instance
+    character_model_1 = Agent(0, 0)
+    character_view_1 = AgentView(character_model_1)
+    character_controller_1 = AgentController(
+        character_model_1, character_view_1)
+
+    # Player 2 instance
+    character_model_2 = Agent(0, 0)
+    character_view_2 = AgentView(character_model_2)
+    character_controller_2 = AgentController(
+        character_model_2, character_view_2)
+
+    # Player list
+    agent_list = [character_model_1, character_model_2]
+    #agent_sprites = pygame.sprite.Group([character_view_1._sprite, \
+    #                                     character_view_2._sprite])
+
+    map_model.attacker_spawn.set_spawns([character_model_1])
+    map_model.defender_spawn.set_spawns([character_model_2])
+    
+    # initialize HUD
+    hud_model = display_model()
+    hud_view = display_view(hud_model)
+
+    # main loop
+    run = True
+    while run:
+
+        # sense inputs (get events)
+        for event in pygame.event.get():  # look for events
+            if event.type == pygame.QUIT:  # quit the game, stop the loop
+                run = False
+            if event.type == track_second:
+                # if a second has passed, reduce the timer
+                if hud_model.timer != 0:
+                    hud_model.timer -= 1
+
+        # update states
+        # create entities
+        # detect interactions
+
+        # movement
+        # check which keys are currently pressed
+        keys = pygame.key.get_pressed()
+        # if no collisions are detected, move characters
+        character_controller_1.move(
+            character_speed, keys, "WASD", map_model._wall_list)
+
+        character_controller_2.move(
+            character_speed, keys, "Arrow", map_model._wall_list)
+      
+        character_controller_1.check_shoot(keys)
+        character_controller_1.check_reload(keys)
+
+        # update stuff
+        # draw backdrop
+        map_view.draw_map()
+
+        # walls will still have collision even if not drawn
+        # map_view.draw_walls()
+  
+        # draw HUD updates
+        hud_view.draw_player_updates(
+            character_model_1, character_model_2, map_view._window)
+        hud_view.draw_game_timer(map_view._window)
+
+        # draw characters
+        character_view_1.draw_agent(map_view._window)
+        character_view_2.draw_agent(map_view._window)
+
+        character_controller_1.update_bullets_test(map_model._wall_list)
+        character_view_1.draw_bullets(map_view._window)
+
+        pygame.display.flip()  # update entire display
+        clock.tick(30)  # reduce framerate to 30
+
+    # print(map_view._window.get_rect()) #check window dimensions
+    pygame.quit()  # after main loop has finished
